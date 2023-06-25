43bd8c91fdf4a18740df7b6b26d8c91d95c5827e
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-06-25 12:10:27 +0300

    Added ParkingController to the CoolParking.WebAPI project

---
+++ CoolParking/CoolParking.WebAPI/Controllers/ParkingController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoolParking.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParkingController : ControllerBase
+    {
+
+    }
+}
