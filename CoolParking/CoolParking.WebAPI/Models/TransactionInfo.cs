451bea3c1db2591d30e177a38ac7f4916695e05e
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-06-25 12:59:25 +0300

    Added TransactionInfo model

---
+++ CoolParking/CoolParking.WebAPI/Models/TransactionInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace CoolParking.WebAPI.Models
+{
+    public class TransactionInfo
+    {
+        public decimal Sum { get; set; }
+        public string? TransactionTime { get; set; }
+        public string? VehicleId { get; set; }
+    }
+}
