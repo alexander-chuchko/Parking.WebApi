6c53adc7b543596d3cab0ca0593cd1629c1c314e
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-06-25 13:02:07 +0300

    Added Parking model

---
+++ CoolParking/CoolParking.WebAPI/Controllers/Parking.cs
@@ -0,0 +1,13 @@
+﻿using CoolParking.WebAPI.Models;
+
+namespace CoolParking.WebAPI.Controllers
+{
+    public class Parking
+    {
+        private static Parking? instance;
+        public List<Vehicle> Vehicles { get; set; }
+        public decimal Balance { get; set; }
+
+        public DateTime? StartTime { get; set; }
+    }
+}
